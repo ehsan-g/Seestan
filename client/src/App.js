723678849fbcae8925d270417a09ad944d7911a5
ehsan <ehsan@say.company>
2021-03-03 19:56:19 -0500

    AppBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
+import Header from './components/nav/Header';
 
-const App = () => <div>Hi</div>;
+// Override the defualt theme from here
+const theme = createMuiTheme({
+  overrides: {
+    // Style sheet name => example Link component ⚛️
+    MuiLink: {
+      // Name of the rule
+      underlineNone: {
+        // Some CSS
+        color: 'black',
+        '&:hover': {
+          color: 'blue',
+          textDecoration: 'none',
+        },
+      },
+    },
+    MuiAppBar: {
+      colorPrimary: {
+        backgroundColor: 'white',
+        color: 'black',
+      },
+    },
+    MuiTypography: {
+      root: {
+        alignContent: 'center',
+        padding: '2px',
+      },
+      subtitle1: {
+        fontSize: '14px',
+        padding: '5px',
+      },
+    },
+    MuiButton: {
+      contained: {
+        backgroundColor: 'black',
+        color: 'white',
+        minWidth: '41px',
+        '&:hover': {
+          color: 'white',
+          backgroundColor: '#b77990',
+          textDecoration: 'none',
+        },
+      },
+      outlined: {
+        minWidth: '41px',
+        '&:hover': {
+          borderColor: 'black',
+        },
+      },
+    },
+  },
+});
 
+const App = () => (
+  <>
+    <ThemeProvider theme={theme}>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </ThemeProvider>
+  </>
+);
 export default App;
---
+++ client/src/components/nav/Header.js
@@ -0,0 +1,241 @@
+import React from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Avatar from '@material-ui/core/Avatar';
+import { Button, Box, Link, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  myButton: {
+    marginRight: '20px',
+    marginLeft: '10px',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    direction: 'rtl',
+    borderRadius: '0',
+    border: '1px solid rgb(229, 229, 229)',
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+      borderColor: '#6c757d',
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    height: '40px',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: '100%',
+    },
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+    '& input::placeholder': {
+      fontSize: '14px',
+    },
+  },
+  inputInput: {
+    padding: theme.spacing(2, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(0.2)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  const preventDefault = (event) => event.preventDefault();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <Button variant="contained">ثبت‌نام</Button>
+            <Button variant="outlined" className={classes.myButton}>
+              ورود
+            </Button>
+            <Typography variant="subtitle1">
+              <Link
+                href="#"
+                component="a"
+                onClick={preventDefault}
+                underline="none"
+              >
+                گزینش‌شده
+              </Link>
+            </Typography>
+            <Typography variant="subtitle1">
+              <Link
+                href="#"
+                component="a"
+                onClick={preventDefault}
+                underline="none"
+              >
+                فروش
+              </Link>
+            </Typography>
+            <Typography variant="subtitle1">
+              <Link
+                href="#"
+                component="a"
+                onClick={preventDefault}
+                underline="none"
+              >
+                خرید
+              </Link>
+            </Typography>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+          <div className={classes.search}>
+            <InputBase
+              placeholder="جستجو با نام هنرمند، گالری، اثر، استایل و غیره"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div>
+            <Avatar
+              alt="Logo"
+              variant="square"
+              src="/static/logo.png"
+              className={classes.large}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      {}
+      {}
+    </div>
+  );
+}
