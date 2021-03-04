2003fd676a9aeb07e39ecec24d6a2244059fb13c
ehsan <ehsan@say.company>
2021-03-04 09:57:23 -0500

    firebase auth

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBwrcTjDht0q5YU5PHgA-ZnneYgns20s9Y',
+  authDomain: 'e-commerce-19e8f.firebaseapp.com',
+  projectId: 'e-commerce-19e8f',
+  storageBucket: 'e-commerce-19e8f.appspot.com',
+  messagingSenderId: '232565550722',
+  appId: '1:232565550722:web:745be823eb068cc2588e9b',
+};
+
+//  Initilize firebase
+firebase.initializeApp(firebaseConfig);
+
+//  export
+export const { auth } = firebase;
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
