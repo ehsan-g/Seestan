40855a7bd5727f3d19ad0a9fba13f9a484a91950
ehsan <ehsan@say.company>
2021-03-20 20:52:19 -0400

    basic api setup

--- backend/artworks/urls.py
+++ backend/artworks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name='routes')
+    path('', views.getRoutes, name='routes'),
+    path('artworks/', views.getArtWorks, name='artWorks'),
+    path('artworks/<str:pk>', views.getTheArtWork, name='theArtWork'),
 ]
--- backend/artworks/views.py
+++ backend/artworks/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
 
 # Create your views here.
 
-
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello', safe=False)
+    routes = [
+        'api/artworks/create',
+        'api/artworks/upload',
+        'api/artworks/<id>/favorite',
+        'api/artworks/top',
+        'api/artworks/<id>',
+        'api/artworks/delete/<id>',
+        'api/artworks/<update>/<id>',
+    ]
+    return Response(routes)
+
+@api_view(['GET'])
+def getArtWorks(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getTheArtWork(request, pk):
+    product = None
+    for theArtWork in products:
+        if theArtWork['_id'] == pk:
+            product = theArtWork
+            break
+
+    return Response(product)
