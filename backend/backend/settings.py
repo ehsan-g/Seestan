faeba37dfc9cd72e81987656cc6cb86487d377db
ehsan <ehsan@say.company>
2021-03-20 23:06:56 -0400

    api connected / axios

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,8 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'corsheaders',
     'artworks.apps.ArtworksConfig',
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import products from '../apis/products';
+import artworksBase from '../apis/artworksBase';
 
 export const fetchOneArtWork = (workId) => {
   const oneArtwork = products.find((p) => p._id === workId);
@@ -8,86 +9,15 @@ export const fetchOneArtWork = (workId) => {
   };
 };
 
-export const fetchAllArtWorks = () => {
-  const allArtworks = products;
-  return {
-    type: 'FETCH_ALL_ARTWORKS',
-    payload: allArtworks,
-  };
+export const fetchAllArtWorks = () => async (dispatch) => {
+  try {
+    const response = await artworksBase.get('api/artworks');
+    console.log(response);
+    dispatch({
+      type: 'FETCH_ALL_ARTWORKS',
+      payload: response.data,
+    });
+  } catch (e) {
+    alert('looks like there is problem with your internet connection');
+  }
 };
-
-// export const fetchNeed = () => async (dispatch) => {
-//   try {
-//     const response = await randomNeed.get(
-//       '/api/v2/public/random/need?_lang=en'
-//     );
-//     dispatch({
-//       type: 'FETCH_NEED',
-//       payload: response.data,
-//     });
-//   } catch (e) {
-//     alert('looks like there is problem with your internet connection');
-//   }
-// };
-
-// export const fetchEthPrice = (needFetchedCost) => async (dispatch) => {
-//   try {
-//     const response = await etherPrice.get('/api/v3/coins/ethereum');
-//     // Due to $ / IRR sever volatility in recent years we are using a constant rate for this version.
-//     const USDtoIRR = 25000;
-//     const needUsdCost = needFetchedCost / USDtoIRR;
-//     const needCostUSD = needUsdCost.toFixed(2);
-//     const ethCurrentPrice = response.data.market_data.current_price.usd;
-//     const needEthCost =
-//       Math.round((needCostUSD / ethCurrentPrice) * 1000000) / 1000000;
-
-//     dispatch({
-//       type: 'FETCH_ETH',
-//       payload: {
-//         ethCurrentPrice,
-//         needEthCost,
-//       },
-//     });
-//   } catch (error) {
-//     alert('looks like there is problem with your internet connection');
-//   }
-// };
-
-// export const connectWallet = (accounts, web3, networkId, nakama) => ({
-//   type: 'CONNECT_WALLET',
-//   payload: {
-//     accounts,
-//     userAccount: accounts[0],
-//     web3,
-//     nakama,
-//     networkId,
-//   },
-// });
-
-// export const activateModal = () => ({
-//   type: 'NAK_MODAL',
-//   payload: true,
-// });
-
-// export const deactivateModal = () => ({
-//   type: 'NO_NAK_MODAL',
-//   payload: false,
-// });
-
-// export const fetchIsOwner = (isOwner) => ({
-//   type: 'IS_OWNER',
-//   payload: isOwner,
-// });
-
-// export const fetchTokenURI = (link) => {
-//   console.log(link);
-//   return {
-//     type: 'LINK',
-//     payload: link,
-//   };
-// };
-
-// export const updateMintButton = (text, status) => ({
-//   type: 'UPDATE_BUTTON_TEXT',
-//   payload: { text, status },
-// });
