11d5187ee3c2a2da7e5bb6da82aab2f9731c7536
ehsan <ehsan@say.company>
2021-04-03 15:51:44 -0400

    user serializer

--- backend/artworks/serializer.py
+++ backend/artworks/serializer.py
@@ -1,6 +1,42 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Artwork
+
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
+
+    # for changing id to _id and keeping the same convention
+    def get__id(self, obj):
+        return obj.id
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+        return name
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+
+    def get_token(sefl, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
 
 
 class ArtworkSerializer(serializers.ModelSerializer):
--- backend/artworks/views.py
+++ backend/artworks/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .products import products
-from .serializer import ArtworkSerializer
+from .serializer import ArtworkSerializer, UserSerializer, UserSerializerWithToken
 
 from .models import Artwork
 # Create your views here.
@@ -14,13 +14,26 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 #  Customizing token claims with JWT / overriding
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims, meaning when token decrypted more data is available check jwt.io
+        token['username'] = user.username
+        token['message'] = 'hello world'
+        return token
+
     def validate(self, attrs):
         data = super().validate(attrs)
-# Customized
-        data['username'] = self.user.username
-        data['email'] = self.user.email
 
+        # data['username'] = self.user.username
+        # data['email'] = self.user.email
+        #  ...
+        # refactored to the following:
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+        
         return data
+
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
@@ -38,6 +51,15 @@ def getRoutes(request):
         'api/artworks/<update>/<id>',
     ]
     return Response(routes)
+
+
+# since we have wrapped this view with rest-framework decorator and changed the default authentication
+# /api/users/profile user is not the same as the user used for /admin
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
