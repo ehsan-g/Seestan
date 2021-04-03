212226722e9b7ceffdb90c47f609a5f5b73c6668
ehsan <ehsan@say.company>
2021-04-03 17:02:05 -0400

    register view

--- backend/artworks/urls.py
+++ backend/artworks/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('users/login', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('', views.getRoutes, name='routes'),
+    path('users/register', views.registerUser, name='register'),
     path('users/profile/', views.getUserProfile, name='users_profile'),
+    path('users/', views.getUsers, name='users'),
     path('artworks/', views.getArtWorks, name='artWorks'),
     path('artworks/<str:pk>', views.getTheArtWork, name='theArtWork'),
 ]
--- backend/artworks/views.py
+++ backend/artworks/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from .products import products
 from .serializer import ArtworkSerializer, UserSerializer, UserSerializerWithToken
-
+from django.contrib.auth.models import User
 from .models import Artwork
+from rest_framework import status
+
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.hashers import make_password
 
 
 #  Customizing token claims with JWT / overriding
-
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
@@ -31,7 +33,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         serializer = UserSerializerWithToken(self.user).data
         for k, v in serializer.items():
             data[k] = v
-        
+
         return data
 
 
@@ -53,12 +55,39 @@ def getRoutes(request):
     return Response(routes)
 
 
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail: User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
 # since we have wrapped this view with rest-framework decorator and changed the default authentication
 # /api/users/profile user is not the same as the user used for /admin
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 
