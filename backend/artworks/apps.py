4e6d3beb36e7f401d0cbae1af5bf743fd32706aa
ehsan <ehsan@say.company>
2021-04-03 17:13:48 -0400

    change username if  email changed

--- backend/artworks/apps.py
+++ backend/artworks/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class ArtworksConfig(AppConfig):
     name = 'artworks'
+
+    def ready(self):
+        import artworks.signals
---
+++ backend/artworks/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import pre_save
+from django.contrib.auth.models import User
+
+# when email is changed user name is changed
+def updateUser(sender, instance, **kwargs):
+    print('Sognal Triggered')
+    user = instance
+    if user.email != '':
+        user.username = user.email
+
+
+pre_save.connect(updateUser, sender=User)
