3c6819e77ca55a2cf9855b66d88a9c4699823fa7
ehsan <ehsan@say.company>
2021-04-26 21:00:45 -0400

    changed and delete some files by mistake

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'CHANGETHIS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 # DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
